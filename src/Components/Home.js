2863e1d7dc66e848f675ea6b12d5326732e43e3f
HaydenDyer <hcdyer0@gmail.com>
2021-10-08 11:56:02 -0400

    move config.js into src

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,50 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import Listing from './Listing';
 import axios from 'axios';
-import API_URL from '../../config';
+import API_URL from '../config';
 
 function Listings(props) {
-	const [listings, setListings] = useState(null);
-	const [loading, setLoading] = useState(true);
+	// const [listings, setListings] = useState(null);
+	// const [loading, setLoading] = useState(true);
 
-	const getListing = async () => {
-		try {
-			const res = await axios.get(`${API_URL}/items`);
-			setListings(res.data);
-		} catch (error) {
-			console.log(error);
-		}
-	};
+	// const getListing = async () => {
+	// 	try {
+	// 		const res = await axios.get(`${API_URL}/items`);
+	// 		setListings(res.data);
+	// 	} catch (error) {
+	// 		console.log(error);
+	// 	}
+	// };
 
 	// On page load a timeout function is called
-	useEffect(() => {
-		const handleLoadingTimeout = setTimeout(() => {
-			if (!listings.length) {
-				setLoading(false);
-			}
-		}, 5000);
+	// useEffect(() => {
+	// 	const handleLoadingTimeout = setTimeout(() => {
+	// 		if (!listings.length) {
+	// 			setLoading(false);
+	// 		}
+	// 	}, 5000);
 		// the getListing() function is invoked to make our API call
-		getListing();
+		// getListing();
 
-		return () => clearTimeout(handleLoadingTimeout);
-	}, []);
+		// return () => clearTimeout(handleLoadingTimeout);
+	// }, []);0
 
 	// UX/UI considerations to provide feedback to the user
-	if (loading && !listings.length) {
+	// if (loading && !listings.length) {
 		// placeholder can do something more dynamic later
-		return <h2>Loading...</h2>;
-	}
-	if (!loading && !listings.length) {
-		return <h2>Oops, something went wrong. Please try again Later!</h2>;
-	}
+	// 	return <h2>Loading...</h2>;
+	// }
+	// if (!loading && !listings.length) {
+	// 	return <h2>Oops, something went wrong. Please try again Later!</h2>;
+	// }
 
 	// Our results are displayed below
 	return (
 		<div>
 			<div>
-				{listings.map((item) => (
+				{/* {listings.map((item) => (
 					<Listing key={item.id} />
-				))}
+				))} */}
 			</div>
 		</div>
 	);
